dbeafdd761594c91e7823176fd8d36173018de24
Guillaume <guigui.fabbi@gmail.com>
2018-11-05 20:16:03 +0100

    Initial commit

--- src/main/java/fr/gwombat/springrewriteurl/SpringRewriteUrlApplication.java
+++ src/main/java/fr/gwombat/springrewriteurl/SpringRewriteUrlApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringRewriteUrlApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(SpringRewriteUrlApplication.class, args);
     }
 }
